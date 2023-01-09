9d65fb130f1571771d70df74c1129751d200ef29
anushkachouhan029 <anushkachouhan029>
2023-01-09 22:47:28 +0530

    emp wage for month

---
+++ uc5-empwageformonths/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAYS = 22;
+        static void Main(string[] args)
+        {
+            //Variables
+
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+            //computation
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage = empWage * NO_OF_WORKING_DAYS;
+            Console.WriteLine("Total working hours : " + empHrs * NO_OF_WORKING_DAYS);
+
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+
+        }
+    }
+}
