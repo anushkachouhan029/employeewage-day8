18f592cbad0906e060c2bdaa07fb9e93d8fa9b68
anushkachouhan029 <anushkachouhan029>
2023-01-09 22:51:19 +0530

    added employee limited wage

--- uc5-empwageformonths/Program.cs
+++ uc5-empwageformonths/Program.cs
@@ -5,37 +5,43 @@
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NO_OF_WORKING_DAYS = 22;
+        public const int NUM_OF_WORKING_DAYS = 22;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
+
             //Variables
 
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
             //computation
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+
             }
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage = empWage * NO_OF_WORKING_DAYS;
-            Console.WriteLine("Total working hours : " + empHrs * NO_OF_WORKING_DAYS);
-
+            Console.WriteLine("Total working days : " + totalWorkingDays + " Total working hours : " + totalEmpHrs);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
 
         }
